7b6ba6333b55112210edb87a72d783f37f03964c
PastryArmor <dvernitskiyi@gmail.com>
2025-02-18 23:19:26 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 8080))  # Use Cloud Run's expected port
+    app.run(host='0.0.0.0', port=port)
